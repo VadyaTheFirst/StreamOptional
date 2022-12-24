09fabae9e8dd523635c6006790290a7acfd628d8
vadym <vadikhide@yandex.ru>
2022-12-24 19:49:24 +0200

    Стримы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+
 public class Main {
+    //private static Comparator< List<Integer>, List<Integer>> minMaxConsumer;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Integer>list=new ArrayList<>();
+        for (Integer i = 1; i < 5; i++) {
+            list.add(i);
+        }
+
+        //Задание 1
+
+
+        System.out.println(list.stream().max(Comparator.naturalOrder()).get());
+        System.out.println(list.stream().min(Comparator.naturalOrder()).get());
+
+
+        //Задание2
+
+        list.stream().filter(integer -> integer % 2 == 0).forEach(System.out::println);
+        list.stream().filter(integer -> integer % 2 == 0).count();
     }
 }
